ce09ba1f79e9e7ac02e4cd1b299b01f8954c715d
Junaid <jahmad095@gmail.com>
2019-06-03 12:50:50 -0400

    chunking

--- getAndPrintChunks.js
+++ getAndPrintChunks.js
@@ -1,25 +1,29 @@
 //import the https module
 var https = require('https');
+var data = '';
 
 var requestOptions = {
     host:'sytantris.github.io',
     path: '/http-examples/step1.html'
 };
 
+ 
+
 //callback function to get  the information through a http response 
 https.get(requestOptions, function(response){
     response.setEncoding('utf8');
 
     //while recieving data 
-    response.on('data', function (data){
+    response.on('data', function (chunk){
+        //console.log(chunk);
+        data += chunk;
+        console.log("NEXT");
 
     });
 
     //when we have recieved all the data
     response.on('end', function(){
-        console.log("resonse stream has ended");
+        console.log(data);
 
-
-        
     });
 });
